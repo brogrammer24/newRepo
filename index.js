48b01f993caafb7c4235420afcf4b62bb5d58a0f
AnishNandi24 <102163207+brogrammer24@users.noreply.github.com>
2022-06-10 21:16:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log("hello")
+document.getElementById("info").addEventListener("submit",(event)=>{
+    event.preventDefault()
+})
+var i=0
+function represent(){
+    var name=document.getElementById("name").value
+    var roll=document.getElementById("roll").value
+    var name=document.getElementById("name").value
+    var name=document.getElementById("name").value
+    var name=document.getElementById("name").value
+
+}
